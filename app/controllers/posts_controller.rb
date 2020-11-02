efc223b6c0979d4c5f53968290dc69a905070cec
Dulan Perera <dulanp18@gmail.com>
2020-11-02 13:48:54 +1300

    index and create actions with views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,37 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!
-  belongs_to :users
+
 
   def index
+    @posts = Post.all
+    @post = Post.new()
+    @user = current_user
   end
 
   def new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :index
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'post#index', as: :authenticated_root
+      root 'posts#index', as: :authenticated_root
     end
 
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :posts
 
 
 end
